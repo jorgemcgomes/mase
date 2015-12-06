fd8a07981e25cdc697be620276ccfdb866705ce8
jorge <jorge@jorge-laptop>
2015-12-06 19:09:37 +0000

    cleanup

--- batchre.sh
+++ batchre.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/down_tog/fit/config.params -f ~/exps/ecal2/down_tog/fit -p problem.landPlacement=1 -p problem.flyingPlacement=1
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/down_sep/fit/config.params -f ~/exps/ecal2/down_sep/fit -p problem.landPlacement=1 -p problem.flyingPlacement=2
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/stable_tog/fit/config.params -f ~/exps/ecal2/stable_tog/fit -p problem.landPlacement=1 -p problem.flyingPlacement=1
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/stable_sep/fit/config.params -f ~/exps/ecal2/stable_sep/fit -p problem.landPlacement=1 -p problem.flyingPlacement=2
+./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/down_tog/nsmix/config.params -f ~/exps/ecal2/down_tog/nsmix -f ~/exps/ecal2/down_tog/moea -f ~/exps/ecal2/down_tog/staged
 
+./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/down_sep/nsmix/config.params -f ~/exps/ecal2/down_sep/nsmix -f ~/exps/ecal2/down_sep/moea -f ~/exps/ecal2/down_sep/staged
+
+./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/stable_sep/nsmix/config.params -f ~/exps/ecal2/stable_sep/nsmix -f ~/exps/ecal2/stable_sep/moea -f ~/exps/ecal2/stable_sep/staged
 
