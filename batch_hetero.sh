48e610088a269a197e61aff4e6bf4c53e4874aac
jorge <jorge@jorge-laptop>
2016-01-05 19:31:55 +0000

    Organisation and cleanup

--- batch_hetero.sh
+++
@@ -1,7 +0,0 @@
-./run.r mase.spec.HeterogeneityCheck -file null -a 5 -f ~/exps/hybrid/herdfinal/s5_f2_p1_hyb_t20 -f ~/exps/hybrid/herdfinal/s5_f2_p1_base
-./run.r mase.spec.HeterogeneityCheck -file null -a 5 -f ~/exps/hybrid/herdfinal/s5_f1_p1_smart_hyb_t20 -f ~/exps/hybrid/herdfinal/s5_f1_p1_smart_base
-./run.r mase.spec.HeterogeneityCheck -file null -a 7 -f ~/exps/hybrid/herdfinal/s7_f2_p2_hyb_t20 -f ~/exps/hybrid/herdfinal/s7_f2_p2_base
-./run.r mase.spec.HeterogeneityCheck -file null -a 7 -f ~/exps/hybrid/herdfinal/s7_f2_p1_smart_hyb_t20 -f ~/exps/hybrid/herdfinal/s7_f2_p1_smart_base
-./run.r mase.spec.HeterogeneityCheck -file null -a 10 -f ~/exps/hybrid/herdfinal/s10_f3_p2_hyb_t20 -f ~/exps/hybrid/herdfinal/s10_f3_p2_base
-./run.r mase.spec.HeterogeneityCheck -file null -a 10 -f ~/exps/hybrid/herdfinal/s10_f3_p1_smart_hyb_t20 -f ~/exps/hybrid/herdfinal/s10_f3_p1_smart_base
-
--- batchre.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/down_tog/nsmix/config.params -f ~/exps/ecal2/down_tog/nsmix -f ~/exps/ecal2/down_tog/moea -f ~/exps/ecal2/down_tog/staged
-
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/down_sep/nsmix/config.params -f ~/exps/ecal2/down_sep/nsmix -f ~/exps/ecal2/down_sep/moea -f ~/exps/ecal2/down_sep/staged
-
-./run.sh mase.stat.BatchReevaluate -r 50 -file ~/exps/ecal2/stable_sep/nsmix/config.params -f ~/exps/ecal2/stable_sep/nsmix -f ~/exps/ecal2/stable_sep/moea -f ~/exps/ecal2/stable_sep/staged
-
